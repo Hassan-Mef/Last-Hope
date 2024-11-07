c2a393d10020647af8ee0a2d513f6b71e33403d4
Hassan Mef <143568256+BSCE23031@users.noreply.github.com>
2024-11-08 01:12:48 +0500

    temp base class testing

--- Last Hope/Last Hope.cpp
+++ Last Hope/Last Hope.cpp
@@ -5,6 +5,7 @@
 #include "Map/Grid.h"
 #include "Map/Map.h"
 #include "Editor/MapEditor.h"
+#include "Enemies/Enemy.h"
 
 int main()
 {
@@ -15,6 +16,7 @@ int main()
 
     // Create the SFML window
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Last Hope");
+	window.setFramerateLimit(20);  // Limit the game to 60 frames per second
 
     std::map<int, std::string> texturePaths = {
         {0, "assets/textures/bg.png"},
@@ -27,6 +29,9 @@ int main()
     Grid grid(10, 12, tileSize);
     Map map(grid, texturePaths);
 	map.loadMap("MapData/map_1.json");
+    Enemy enemy("assets/Sprite/Attack.png", 100, 10, 12, 20);
+	enemy.setPosition(sf::Vector2f(tileSize * 2, tileSize * 2));
+
      //MapEditor editor(grid, map);
 
     while (window.isOpen()) {
@@ -35,10 +40,24 @@ int main()
             if (event.type == sf::Event::Closed)
                window.close();
          //  editor.handleInput(event, window);
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Left) {
+                    enemy.move(window);
+                }
+
+            }
         }
+
 
         window.clear(sf::Color::Black);
         map.draw(window);
+        
+		enemy.render(window);
+
+        enemy.attack(); // Animation for enemy attacking
+
+		
+		
 
       //  editor.draw(window);
         window.display();
